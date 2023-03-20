f29503b630d22f7d9082f3c8eca93190e9a3b802
LMC <101943576+april808@users.noreply.github.com>
2023-03-20 18:36:54 +0800

    刪除id綁定與多餘備註

--- README.md
+++ README.md
@@ -13,5 +13,11 @@ CodePen URL:
 
 參考：
 - [Before & After Image](https://codepen.io/josetxu/pen/mJbmZY)
+- [V I - Before and After Image Comparison Slider using CSS Variables. SUPER simple.](https://codepen.io/mark_sottek/pen/wvmveMG)
 - [原生JS | 滑動前後對比圖片，Before/After效果【一起學前端](https://www.youtube.com/watch?v=EAc7PktCsY0)
 - [「前後對比」效果｜只用一行JS](https://www.youtube.com/watch?v=AmEo-BKTyNo)
+- [Before/After Image Slider Comparison (HTML, CSS, and JavaScript)](https://www.youtube.com/watch?v=dzqDU9efnnk)
+後續參考： 
+- [MDN-<input type=“range”>](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range)
+- [MDN-this](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this)
+- [[JavaScript] getElements ByClassName 取得對應的 Class 物件進行操作](https://zwh.zone/javascript-getelementsbyclassname—e5-8f-96-e5-be-97-e5-b0-8d-e6-87-89-e7-9a-84-class—e7-89-a9-e4-bb-b6-e9-80-b2-e8-a1-8c-e6-93-8d-e4-bd-9c/)
--- dist/script.js
+++ dist/script.js
@@ -1,12 +1,4 @@
 console.clear()
-// const value = document.querySelector("#value")
-// const input = document.querySelector("#pi_input")
-// value.textContent = input.value
-// input.addEventListener("input", (event) => {
-//   value.textContent = event.target.value
-//   console.log(event.target);
-// })
-
 
 const valueA = document.querySelector("#valueA")
 
@@ -17,14 +9,10 @@ document.addEventListener("input", (event) => {
   
   let afterSlider = document.getElementsByClassName('before_after_slider');
   let beforeImg = document.getElementsByClassName('before_after');
-  // let beforeImg_div = document.querySelector('.before_after');
-  
-  console.log(beforeImg,"img")
-  console.log(afterSlider,"range")
   
   if(checkSlider){
     for(var i=0;i<beforeImg.length;i++){
-      if(afterSlider[i].id == this_slider.id){
+      if(afterSlider[i] == this_slider){
         beforeImg[i].style.width = this_slider.value + "%";
       }
     }
@@ -32,13 +20,6 @@ document.addEventListener("input", (event) => {
   }
 
   // console.log(checkSlider)
-  // console.log(event.target.id);
+  console.log(event.target.id);
   // console.log(beforeImg_div.style)
 })
-
-
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this
-
-// https://zwh.zone/javascript-getelementsbyclassname--e5-8f-96-e5-be-97-e5-b0-8d-e6-87-89-e7-9a-84-class--e7-89-a9-e4-bb-b6-e9-80-b2-e8-a1-8c-e6-93-8d-e4-bd-9c/
